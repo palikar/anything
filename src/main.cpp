e0e380c9e6902b980638c13a9ec427fadd3d010f
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-04-08 10:21:16 +0200

    Adding source files and keycode definitions

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include <GL/glew.h>
+
+#include "application/application.hpp"
+#include "commons.hpp"
+
+
+namespace ay {
+
+GLenum GLCheckError()
+{
+    return glGetError();
+}
+
+bool GLLogCall(const char* function, const char* file, int32_t line)
+{
+    GLenum error = GLCheckError();
+    if (error != GL_NO_ERROR)
+    {
+        std::cout << "[OpenGL Error] (" << error << "): "<< function << " " << file << ":" << line << "\n";
+        return false;
+    }
+    return true;
+}
+
+}
+
+
+
+
+int main()
+{
+    ay::Application app{1024, 768, nullptr};
+    return app.run();
+    
+    return 0;
+}
