bc4c49aa608092e0a740bd1b97c2036732cd3c9b
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-04-09 12:50:37 +0200

    Starting up some work on the scene

---
+++ include/engine/scene.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <memory>
+
+
+#include "engine/component.hpp"
+#include "engine/entity.hpp"
+
+
+
+namespace ay
+{
+
+class Scene3D
+{
+  private:
+
+
+  public:
+    
+    
+
+};
+
+
+
+}
