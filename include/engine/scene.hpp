ba059c3106a9aed63e4f791302002ff32bfe02d1
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-04-10 13:59:37 +0200

    Adding the scene class

--- include/engine/scene.hpp
+++ include/engine/scene.hpp
@@ -26,24 +26,52 @@ class Scene3D
     std::vector<EntityPtr> m_entities;
     Camera m_camera;
     GameBase* m_game;
+    ShaderPtr m_base_shader;
 
   public:
     
     Scene3D()
     {}
 
-    void init(){
-
-    }
+    void init(GameBase* t_game);
 
     Entity* add(EntityPtr t_entity)
-    {}
+    {
+        m_entities.push_back(std::move(t_entity));
+        return m_entities.back().get();
+    }
     
-    void update(double)
-    {}
+    void update(double dt)
+    {
+        for (auto& object : m_entities) {
+            object->update(dt);
+        }
+        m_base_shader->bind();
+        m_base_shader->set("projection_matrix", m_camera.view_projection());
+    }
 
-    void render(Renderer&)
-    {}
+    void render(Renderer& render_api)
+    {
+        m_base_shader->bind();
+        
+        // std::cout << glm::to_string(trans_comp->transform.get_tranformation()) << "\n";
+        // std::cout << glm::to_string(m_camera.view_projection()) << "\n";
+
+        for (auto& object : m_entities) {
+
+            auto mesh_comp = object->component<MeshComponent>();
+            auto trans_comp = object->component<TransformComponent>();
+            
+            if (mesh_comp && trans_comp ) {
+
+                m_base_shader->set("model_matrix", trans_comp->transform.get_tranformation());
+                mesh_comp->mesh.geometry()->bind();
+                render_api.draw_indexed(mesh_comp->mesh.geometry());
+                
+            }
+            
+        }        
+    }
 
     Camera& camera()
     {
---
+++ src/scene.cpp
@@ -0,0 +1,21 @@
+
+#include "engine/scene.hpp"
+#include "engine/game_base.hpp"
+
+namespace ay {
+
+
+void Scene3D::init(GameBase* t_game)
+{
+    m_game = t_game;
+    m_base_shader = m_game->shaders().load("simple");
+
+    m_base_shader->bind();
+    m_base_shader->set("red_channel", 0.3f);
+    m_base_shader->set("projection_matrix", m_camera.view_projection());
+    std::cout << "initing scene" << "\n";
+
+}
+
+    
+}
