aa5dd9b07dc248e71a6bd23529b29fe7b638f7c8
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-04-10 13:59:58 +0200

    Setting up the meshes

--- include/graphics/mesh_factory.hpp
+++ include/graphics/mesh_factory.hpp
@@ -1,19 +1,58 @@
 #pragma once
 
-
 #include <vector>
 #include <memory>
-#include <utiliy>
+#include <utility>
 
-#include "rendering/vertex.hpp"
-#include "rendering/vertex_array.hpp"
+#include "rendering/buffer_factory.hpp"
 
 #include "graphics/mesh.hpp"
 
 #include "commons.hpp"
 
+#include <glm/glm.hpp>
 
 namespace ay
 {
+
+
+inline Mesh create_cube(){
+
+    auto buffers = make_vertex_array(
+        make_index_buffer({
+                {0, 1, 2},
+                {2, 3, 0},
+            
+                {1, 5, 6},
+                {6, 2, 1},
+            
+                {7, 6, 5},
+                {5, 4, 7},
+            
+                {4, 0, 3},
+                {3, 7, 4},
+            
+                {4, 5, 1},
+                {1, 0, 4},
+                
+                {3, 2, 6},
+                {6, 7, 3}
+            }),
+        make_vertex_buffer({
+                {-1.0, -1.0,  1.0},
+                    {1.0, -1.0,  1.0},
+                    {1.0,  1.0,  1.0},
+                    {-1.0,  1.0,  1.0},
+
+                    {-1.0, -1.0, -1.0},
+                    {1.0, -1.0, -1.0},
+                    {1.0,  1.0, -1.0},
+                    {-1.0,  1.0, -1.0}
+                }));
+
+    return Mesh(std::move(buffers));
+
+}
+
 
 }
