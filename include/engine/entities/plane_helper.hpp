2e29f4ee3ddcd7f7c816faf33c17e8b26a627835
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-04-18 20:09:34 +0200

    Cleaing up the plane helper

--- include/engine/entities/plane_helper.hpp
+++ include/engine/entities/plane_helper.hpp
@@ -34,20 +34,18 @@ class PlaneHelper : public Entity
     cmp::TransformComponent *m_transform;
     cmp::GroupComponent* m_children;
 
-    glm::mat4 m_proj_mat;
-    glm::mat4 m_proj_mat_inv;
+    glm::vec3 m_color;
+    size_t m_size;
 
   public:
-    PlaneHelper(glm::vec3 color = {1.0f, 1.0f, 1.0f}, size_t size = 1)
-        : m_proj_mat(), m_proj_mat_inv(glm::inverse(m_proj_mat))
+    PlaneHelper(glm::vec3 color = {1.0f, 0.0f, 0.0f}, size_t size = 5)
+        : m_color(color), m_size(size)
     {
 
         m_transform     = add_component(std::make_unique<cmp::TransformComponent>());
         m_children = add_component(std::make_unique<cmp::GroupComponent>());
 
         init_points();
-
-        // m_transform->transform.scale() = glm::vec3(20,20,20);
     }
 
     void init_points()
@@ -79,17 +77,20 @@ class PlaneHelper : public Entity
                                                0, 0, 0,
                                                0, 0, 1} , 3);
 
-        auto mat = grph::solid_color(1.0f, 0.0f, 0.0f);
+
+        auto mat = grph::solid_color(m_color);
         grph::MaterialBuilder::from_existing(mat.get())
             .opacity(0.2)
             .enable_blending()
             .alpha_blending();
-            
+
         cmp::add_children(
             this,
-            line_segments_entity({ std::move(lines_geom),  grph::solid_color(1.0f, 0.0f, 0.0f)}),
+            line_segments_entity({ std::move(lines_geom),  grph::solid_color(m_color)}),
             mesh_entity({ std::move(quad_geom), std::move(mat) })
             );
+
+        m_transform->transform.scale() = glm::vec3(m_size*0.5f, m_size*0.5f, 1);
 
     }
 
