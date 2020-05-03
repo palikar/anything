7924360514bbc6f8bb6dee4453593a699c801035
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-05-03 14:43:59 +0200

    adding integer track

--- include/engine/animation/tracks/number.hpp
+++ include/engine/animation/tracks/number.hpp
@@ -4,9 +4,9 @@
 
 namespace ay::anim
 {
+
 static float inter_num(float a, float b, float factor)
 {
-
     return a * (1.0f - factor) + b * factor;
 }
 
@@ -14,10 +14,26 @@ using NumTrack = anim::Track<float, decltype(inter_num)>;
 template<>
 NumTrack::fun_type NumTrack::fun = &inter_num;
 
-
-NumTrack::ptr_type vector_track(std::vector<float> nums, std::vector<float> times)
+NumTrack::ptr_type num_track(std::vector<float> nums, std::vector<float> times)
 {
     auto p = std::make_unique<NumTrack>(std::move(nums), std::move(times));
+    return p;
+}
+
+
+
+static int inter_int(int a, int b, float factor)
+{
+    return factor > 0.5f ? a : b;
+}
+
+using IntTrack = anim::Track<int, decltype(inter_int)>;
+template<>
+IntTrack::fun_type IntTrack::fun = &inter_int;
+
+IntTrack::ptr_type int_track(std::vector<int> nums, std::vector<float> times)
+{
+    auto p = std::make_unique<IntTrack>(std::move(nums), std::move(times));
     return p;
 }
 
