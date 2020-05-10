f8ca5b493db165e3d90af9d7299e1932e8d4cc83
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-05-10 12:34:26 +0200

    Adding variations for the particles

--- include/engine/particle_system/particles/quad_particle.cpp
+++ include/engine/particle_system/particles/quad_particle.cpp
@@ -10,6 +10,8 @@ namespace ay::part
 grph::Geometry QuadParticle::geometry()
 {
     auto gem = grph::plane_geometry(1, 1, 1, 1);
+    // auto gem = grph::cube_geometry();
+    // gem.rotate_y(glm::radians(90.0));
     gem.drop_attribute("uv");
     gem.drop_attribute("normal");
     gem.drop_attribute("tangents");
@@ -21,10 +23,12 @@ std::array<std::pair<rend::VertexBuffer *, size_t>, QuadParticle::per_instance_b
   QuadParticle::init_buffers(grph::Geometry &geom, size_t t_particle_count)
 {
 
-    auto position_buf =
-      std::make_unique<rend::VertexBuffer>(sizeof(glm::vec3) * t_particle_count);
+    auto position_buf = std::make_unique<rend::VertexBuffer>(
+      (sizeof(glm::vec4) + sizeof(float)) * t_particle_count);
+
     position_buf->set_layout(rend::BufferLayout(
-      { rend::BufferElement{ "position", rend::ShaderDataType::Float3, true } }));
+      { rend::BufferElement{ "position_scale", rend::ShaderDataType::Float4, true },
+        rend::BufferElement{ "angle", rend::ShaderDataType::Float, true } }));
 
     auto color_buf =
       std::make_unique<rend::VertexBuffer>(sizeof(glm::vec4) * t_particle_count);
@@ -34,7 +38,7 @@ std::array<std::pair<rend::VertexBuffer *, size_t>, QuadParticle::per_instance_b
     auto pos   = geom.gl_buffers()->add_vertex_buffer(std::move(position_buf));
     auto color = geom.gl_buffers()->add_vertex_buffer(std::move(color_buf));
 
-    return { std::make_pair(pos, 3), std::make_pair(color, 4) };
+    return { std::make_pair(pos, 5), std::make_pair(color, 4) };
 }
 
 void QuadParticle::update_buffers(
@@ -42,9 +46,11 @@ void QuadParticle::update_buffers(
   std::array<std::vector<float>, per_instance_buffers> &data)
 {
 
-    data[0][index * 3 + 0] = position.x;
-    data[0][index * 3 + 1] = position.y;
-    data[0][index * 3 + 2] = position.z;
+    data[0][index * 5 + 0] = position.x;
+    data[0][index * 5 + 1] = position.y;
+    data[0][index * 5 + 2] = position.z;
+    data[0][index * 5 + 3] = 1.0f;
+    data[0][index * 5 + 4] = 3.14156592 / 4;
 
     data[1][index * 4 + 0] = color.r;
     data[1][index * 4 + 1] = color.g;
@@ -54,7 +60,10 @@ void QuadParticle::update_buffers(
 
 void QuadParticle::init_material(QuadParticle::material_type *material)
 {
-    grph::MaterialBuilder::from_existing(material).addative_blending().enable_blending();
+    grph::MaterialBuilder::from_existing(material)
+      .addative_blending()
+      .enable_blending()
+      .both_side();
 }
 
 void QuadParticle::update(float dt)
--- resources/shaders/quad_particle.vert
+++ resources/shaders/quad_particle.vert
@@ -1,8 +1,9 @@
 #version 460 core
 
 layout (location = 0) in vec3 i_pos;
-layout (location = 1) in vec3 i_position;
-layout (location = 2) in vec4 i_color;
+layout (location = 1) in vec4 i_position;
+layout (location = 2) in float i_angle;
+layout (location = 3) in vec4 i_color;
 
 out vec4 col;
 
@@ -16,24 +17,23 @@ void main() {
     col = i_color;
         
     mat4 particle_model = mat4(1.0);
-    particle_model[3].xyz = i_position;
+    particle_model[3].xyz = i_position.xyz;
 
-    mat4 view = view_matrix;
-
-    view[0][0] = 1.0;
-    view[1][0] = 0.0;
-    view[2][0] = 0.0;
-
-    view[0][1] = 0.0;
-    view[1][1] = 1.0;
-    view[2][1] = 0.0;    
-
+    mat4 view = view_matrix *  particle_model;
+        
+    view[0][0] = 1.0  * cos(i_angle) * i_position.w;
+    view[0][1] = sin(i_angle) * i_position.w;
     view[0][2] = 0.0;
-    view[1][2] = 0.0;
-    view[2][2] = 1.0;
 
-    vec4 pos = vec4(projection_matrix * view * particle_model *  vec4(i_pos, 1.0));
+    view[1][0] = -sin(i_angle) * i_position.w;
+    view[1][1] = 1.0 * cos(i_angle) * i_position.w;
+    view[1][2] = 0.0;    
+
+    view[2][0] = 0.0;
+    view[2][1] = 0.0;
+    view[2][2] = 1.0 * i_position.w;
     
+    vec4 pos = vec4(projection_matrix * view * vec4(i_pos, 1.0));
     gl_Position = pos;
 
 
